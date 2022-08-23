227197e239d63eb648784e3c65de6ae2e4b8dc57
Solus <yannis2004@live.com>
2022-08-22 19:13:26 -0500

    Updated Files.

--- Datavis/Assets/ArrayVisuals.cs
+++ Datavis/Assets/ArrayVisuals.cs
@@ -9,6 +9,17 @@ public class ArrayVisuals : MonoBehaviour
     void OnDrawGizmos()
     {
         int width = myArray.Length;
+        int currentX = 0;
+        int currentY = 0;
+
+        for(int x = 0; x < myArray.Length; x++)
+        {
+            Gizmos.DrawLine(new Vector2(currentX, currentY), new Vector2(currentX + 1, currentY));
+            Gizmos.DrawLine(new Vector2(currentX + 1, currentY), new Vector2(currentX + 1, currentY + 1));
+            Gizmos.DrawLine(new Vector2(currentX + 1, currentY + 1), new Vector2(currentX, currentY + 1));
+            currentX++;
+        }
         Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + width, transform.position.y));
+
     }
 }
