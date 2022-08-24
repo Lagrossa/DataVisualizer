b9ee1b7aeca6cfe1b8d87086909e7ccf8527ff30
Solus <yannis2004@live.com>
2022-08-23 21:44:46 -0500

    Updated Files.

--- Datavis/Assets/ArrayVisuals.cs
+++ Datavis/Assets/ArrayVisuals.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ArrayVisuals : MonoBehaviour
 {
+    public float radius = .055f;
     public int[] myArray = { 9,7,4,3,6,5,2,1,8,0 };
 
     void OnDrawGizmos()
@@ -18,6 +19,7 @@ public class ArrayVisuals : MonoBehaviour
             Gizmos.DrawLine(new Vector2(currentX, currentY), new Vector2(currentX + 1, currentY));
             Gizmos.DrawLine(new Vector2(currentX + 1, currentY), new Vector2(currentX + 1, currentY + 1));
             Gizmos.DrawLine(new Vector2(currentX + 1, currentY + 1), new Vector2(currentX, currentY + 1));
+            Gizmos.DrawSphere(new Vector2(currentX + 1 - currentX, currentY + 1 - currentY), radius);
             currentX++;
         }
         Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + width, transform.position.y));
