9e51680008413e4a826a1247b8fca286fdfb6527
Solus <yannis2004@live.com>
2022-08-23 22:05:37 -0500

    Updated Files.

--- Datavis/Assets/ArrayVisuals.cs
+++ Datavis/Assets/ArrayVisuals.cs
@@ -6,7 +6,8 @@ public class ArrayVisuals : MonoBehaviour
 {
     public float radius = .055f;
     public int[] myArray = { 9,7,4,3,6,5,2,1,8,0 };
-
+    public int DU; // Distance Unit
+    public int scalar; // Scales centers to DU.
     void OnDrawGizmos()
     {
         int width = myArray.Length;
@@ -16,16 +17,17 @@ public class ArrayVisuals : MonoBehaviour
         for(int x = 0; x < myArray.Length; x++)
         {
             Gizmos.color = Color.gray;
-            Gizmos.DrawLine(new Vector2(currentX, currentY + 1), new Vector2(currentX, currentY));
-            Gizmos.DrawLine(new Vector2(currentX, currentY), new Vector2(currentX + 1, currentY));
-            Gizmos.DrawLine(new Vector2(currentX + 1, currentY), new Vector2(currentX + 1, currentY + 1));
-            Gizmos.DrawLine(new Vector2(currentX + 1, currentY + 1), new Vector2(currentX, currentY + 1));
+            Gizmos.DrawLine(new Vector2(currentX, currentY + DU), new Vector2(currentX, currentY));
+            Gizmos.DrawLine(new Vector2(currentX, currentY), new Vector2(currentX + DU, currentY));
+            Gizmos.DrawLine(new Vector2(currentX + DU, currentY), new Vector2(currentX + DU, currentY + DU));
+            Gizmos.DrawLine(new Vector2(currentX + DU, currentY + DU), new Vector2(currentX, currentY + DU));
             Gizmos.color = Color.red;
-            Vector2 center = new Vector2(((currentX + 1) + currentX) / 2f, ((currentY + 1) + currentY)) / 2f;
-        Gizmos.DrawSphere(center, radius);
+            Vector2 center = new Vector2((currentX + DU + currentX), ((currentY + DU) + currentY)) / 2f; // Center of each square (???)
+            Debug.Log($"{center} current center should be" + "(" + (currentX + DU + currentX) / 2f + ")");
+            Gizmos.DrawSphere(center, radius);
             currentX++;
         }
-        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + width, transform.position.y));
+      
 
     }
 }
